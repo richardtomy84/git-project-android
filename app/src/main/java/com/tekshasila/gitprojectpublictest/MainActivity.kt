863a1c396c9015ed805048cf545f786e18ec9320
dm <abf@gmail.com>
2020-12-09 12:01:11 +0530

    First Comit Version

---
+++ app/src/main/java/com/tekshasila/gitprojectpublictest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.tekshasila.gitprojectpublictest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //git check
+    }
+}
